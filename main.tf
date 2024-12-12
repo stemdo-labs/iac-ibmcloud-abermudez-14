e5181e00b1afa72cabc9cd18311e3babb66049e6
abermudez-14 <abermudez@stemdo.io>
2024-12-12 12:36:28 +0100

    Provider

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 terraform {
-  provider_meta "ib" {
-    
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
   }
+}
+
+# Configure the IBM Provider
+provider "ibm" {
+  region = "us-south"
 }

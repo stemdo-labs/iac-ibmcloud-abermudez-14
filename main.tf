d7afee791862a65d26cb4cfe56b41214d225445b
abermudez-14 <abermudez@stemdo.io>
2024-12-12 13:52:21 +0100

    IBM

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "ibm" {
   ibmcloud_api_key="03fU7W2VX2zH2oC9JW7Asj7DV96j9jUG56MaMI0r8PTx"
 }
 
+resource "ibm_resource_group" "abermudez" {
+  name = "abermudez"
+}
+
 resource "ibm_is_vpc" "EJEMPLOVPC" {
   name = "example-vpc-abermudez"
   resource_group = "4364ced224cf420fa07d8bf70a8d70df"

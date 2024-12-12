5c0b663f428a95dee200dea581cd48122872beb8
abermudez-14 <abermudez@stemdo.io>
2024-12-12 13:12:07 +0100

    IBM

--- main.tf
+++ main.tf
@@ -10,8 +10,10 @@ terraform {
 # Configure the IBM Provider
 provider "ibm" {
   region = "us-south"
+  ibmcloud_api_key=var.api_key
 }
 
-resource "ibm_resource_group" "resourceGroupabermudez" {
-  name     = "abermudez-rg"
+resource "ibm_is_vpc" "EJEMPLOVPC" {
+  name = "example-vpc-abermudez"
+  resource_group = "4364ced224cf420fa07d8bf70a8d70df"
 }

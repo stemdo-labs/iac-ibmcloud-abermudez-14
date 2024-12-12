f0551446a5f50b07a1c7b5f7b60b741bd8ae6fcf
abermudez-14 <abermudez@stemdo.io>
2024-12-12 14:29:29 +0100

    IBM

--- main.tf
+++ main.tf
@@ -13,7 +13,22 @@ provider "ibm" {
   ibmcloud_api_key=var.api_key
 }
 
-resource "ibm_is_vpc" "EJEMPLOVPC" {
+resource "ibm_is_vpc" "vpc_abermudez" {
   name = "example-vpc-abermudez"
   resource_group = var.resource_group
+}
+
+resource "ibm_is_vpc_routing_table" "rtable_abermudez" {
+  name = "example-routing-table"
+  vpc  =  ibm_is_vpc.vpc_abermudez.id
+}
+
+resource "ibm_is_subnet" "subnet_abermudez" {
+  name            = "example-subnet"
+  vpc             = ibm_is_vpc.vpc_abermudez.id
+  zone            = "eu-gb"
+  ipv4_cidr_block = "10.0.1.0/16"
+  routing_table   = ibm_is_vpc_routing_table.rtable_abermudez
+  
+
 }
